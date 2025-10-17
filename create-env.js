b7d87783e4d77627935cf447a0373716e83dfea4
Colin Hunwald <colin.hunwald@gmail.com>
2025-10-17 07:19:19 +0200

    create-env.js change

--- create-env.js
+++ create-env.js
@@ -29,11 +29,14 @@ console.log('env.js file generated successfully.');
 console.log(`Copying static assets to ${outputDir}...`);
 
 const assetsToCopy = [
-    'index.html',
     'ETAR_H-IITB_512.png',
     'ETAR_H.png',
     'logo.jpg'
 ];
+
+// Copy etar_rendszer.html to public/index.html
+fse.copySync('etar_rendszer.html', path.join(outputDir, 'index.html'));
+console.log('- Copied etar_rendszer.html to index.html');
 
 assetsToCopy.forEach(file => {
     if (fs.existsSync(file)) {
